03ad2425fa7c9713c1988d8bd8966bd70affd755
lucas.moreno <lfmoreno5891@gmail.com>
2024-12-03 11:52:17 -0300

    Add users.

--- controller/users_controller.py
+++ controller/users_controller.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 
 users_router = APIRouter(prefix="/users")
 
+user = [{"username": "Rick"}, {"username": "Morty"}]
+
 @users_router.get("/")
 async def get_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+    return user
